0f1b260edcabee543f8fb7bd628326f3e64a88f9
Brslvskyy <olegbit87@gmail.com>
2023-05-16 23:20:35 +0300

    init

---
+++ src/Login.tsx
@@ -0,0 +1,18 @@
+import { Button, Text, TextInput, View } from "react-native/types"
+
+const Login = () => {
+    return (
+        <View>
+            <Text>
+                Enter in system
+            </Text>
+            <TextInput />
+            <TextInput />
+            <Button title="Log in" />
+            <Text>
+                Sing in
+            </Text>
+            
+        </View>
+    )
+}
